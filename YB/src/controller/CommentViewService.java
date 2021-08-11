bd94f9718e61f12fe55c0addd7d706d7fdfac7f1
muyaho <lch980628@gmail.com>
2021-08-11 10:48:39 +0900

    수정

--- YB/src/controller/CommentViewService.java
+++ YB/src/controller/CommentViewService.java
@@ -23,6 +23,7 @@ public class CommentViewService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Á¢¼Ó");
 		Gson gson = new Gson();
 	    response.setCharacterEncoding("UTF-8");
 	    PrintWriter out = response.getWriter();
